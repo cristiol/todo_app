dc8dc8074d8a608d06fc14eec9c20d13fd918946
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-02-14 15:12:51 +0200

    added authentication

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='list'),
     path('update_task/<str:pk>/', views.update_task, name='update_task'),
-    path('delete_task/<str:pk>/', views.delete_task, name='delete_task')
+    path('delete_task/<str:pk>/', views.delete_task, name='delete_task'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('logout', views.logout, name='logout'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import *
+from django.contrib.auth.models import auth, User
+from django.contrib import messages
 
 
+@login_required(login_url='login')
 def index(request):
     tasks = Tasks.objects.all()
 
@@ -19,6 +23,7 @@ def index(request):
     return render(request, 'tasks/list_todo.html', context)
 
 
+@login_required(login_url='login')
 def update_task(request, pk):
 
     task = Tasks.objects.get(id=pk)
@@ -37,6 +42,7 @@ def update_task(request, pk):
     return render(request, 'tasks/update_task.html', context)
 
 
+@login_required(login_url='login')
 def delete_task(request, pk):
     item = Tasks.objects.get(id=pk)
 
@@ -47,5 +53,57 @@ def delete_task(request, pk):
     context = {'item': item}
 
     return render(request, 'tasks/delete.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('list')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            redirect('list')
+
+    return render(request, 'tasks/login.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'username taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                return redirect('/')
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('signup')
+
+    return render(request, 'tasks/signup.html')
+
+
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    return redirect('logout')
+
 
 
